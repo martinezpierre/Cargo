a3567e673e56014413487ebc6a96ebbe79a3ed20
unknown <pi.martinez@J001-0135-05.etudiant.rubika-edu.com.local>
2016-05-12 15:16:48 +0200

    first push

---
+++ Source/Cargo/Cargo.Build.cs
@@ -0,0 +1,11 @@
+// Copyright 1998-2016 Epic Games, Inc. All Rights Reserved.
+
+using UnrealBuildTool;
+
+public class Cargo : ModuleRules
+{
+	public Cargo(TargetInfo Target)
+	{
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore" });
+	}
+}
